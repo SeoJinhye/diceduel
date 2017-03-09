2709e8327aa9c8616ad77d24d3a00720fa8361f2
SeoJinhye <jyoon@sjnma.org>
2017-03-09 10:49:05 -0600

    [sound added]

--- diceduel.py
+++ diceduel.py
@@ -1,12 +1,17 @@
 import random
+import pygame
+from pygame import mixer
 P1 = random.randint(1,6)
 P2 = random.randint(1,6)
 P1win = 0
 P2win = 0
 
+mixer.init()
+s = mixer.Sound('bgm.wav')
 Start = raw_input("write start to start the game")
 
 if(Start == "start"):
+	s.play()
 	while(P1win != 3 or P2win != 3):
 		print("P1 got:" + str(P1) + " P2 got:" + str(P2))
 		if(P1 > P2):
@@ -23,6 +28,7 @@ if(Start == "start"):
 		print("Current score: " + str(P1win) + " : " +str(P2win))
 		Start = raw_input("write start to start the game")
 		if(Start == "start"):
+			s.play()
 			P1 = random.randint(1,6)
 			P2 = random.randint(1,6)
 	if(P1win > P2win):
